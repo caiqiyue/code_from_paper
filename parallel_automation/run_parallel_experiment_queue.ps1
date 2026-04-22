46c4c3f2882a5ac341dedc7f86a903f2512483ed
caiqiyue <1019751142@qq.com>
2026-04-22 15:03:20 +0800

    delete

--- parallel_automation/run_parallel_experiment_queue.ps1
+++
@@ -1,7 +0,0 @@
-$root = (Resolve-Path (Join-Path $PSScriptRoot '..')).Path
-Set-Location -LiteralPath $root
-$log = Join-Path $root 'parallel_automation\parallel_experiment_queue.scheduler.log'
-while ($true) {
-    & 'D:\python311\python.exe' (Join-Path $root 'parallel_automation\old_experiment_queue.py') *>> $log
-    Start-Sleep -Seconds 1800
-}
